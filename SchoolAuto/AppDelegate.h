02dd14871d33f2c65d817db76c78f49f555710a0
Arjun <arjunvulapu@gmial.com>
2019-05-03 14:38:24 +0530

    Initial Commit

---
+++ SchoolAuto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchoolAuto
+//
+//  Created by Apple on 03/05/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SchoolAuto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchoolAuto
+//
+//  Created by Apple on 03/05/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
